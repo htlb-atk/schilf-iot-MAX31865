11e8147c875cab5d5a86307fb38d42e588c0928c
Kurt Albrecht <26598013+kurta241@users.noreply.github.com>
2017-12-08 12:51:01 +0100

    fix syntax error

--- lib/MAX31865.py
+++ lib/MAX31865.py
@@ -4,35 +4,32 @@ import math
 import time
 
 class MAX31865():
-    
-### Register constants, see data sheet for info.
-# Read Addresses
-MAX31865_REG_READ_CONFIG  = 0x00
-MAX31865_REG_READ_RTD_MSB = 0x01
-MAX31865_REG_READ_RTD_LSB = 0x02
-MAX31865_REG_READ_HFT_MSB = 0x03
-MAX31865_REG_READ_HFT_LSB = 0x04
-MAX31865_REG_READ_LFT_MSB = 0x05
-MAX31865_REG_READ_LFT_LSB = 0x06
-MAX31865_REG_READ_FAULT   = 0x07
 
-# Write Addresses
-MAX31865_REG_WRITE_CONFIG  = 0x80
-MAX31865_REG_WRITE_HFT_MSB = 0x83
-MAX31865_REG_WRITE_HFT_LSB = 0x84
-MAX31865_REG_WRITE_LFT_MSB = 0x85
-MAX31865_REG_WRITE_LFT_LSB = 0x86
+   ### Register constants, see data sheet for info.
+   # Read Addresses
+   MAX31865_REG_READ_CONFIG  = 0x00
+   MAX31865_REG_READ_RTD_MSB = 0x01
+   MAX31865_REG_READ_RTD_LSB = 0x02
+   MAX31865_REG_READ_HFT_MSB = 0x03
+   MAX31865_REG_READ_HFT_LSB = 0x04
+   MAX31865_REG_READ_LFT_MSB = 0x05
+   MAX31865_REG_READ_LFT_LSB = 0x06
+   MAX31865_REG_READ_FAULT   = 0x07
 
-# Configuration Register
-MAX31865_CONFIG_50HZ_FILTER = 0x01
-MAX31865_CONFIG_CLEAR_FAULT = 0x02
-MAX31865_CONFIG_3WIRE       = 0x10
-MAX31865_CONFIG_ONE_SHOT    = 0x20
-MAX31865_CONFIG_AUTO        = 0x40
-MAX31865_CONFIG_BIAS_ON     = 0x80
+   # Write Addresses
+   MAX31865_REG_WRITE_CONFIG  = 0x80
+   MAX31865_REG_WRITE_HFT_MSB = 0x83
+   MAX31865_REG_WRITE_HFT_LSB = 0x84
+   MAX31865_REG_WRITE_LFT_MSB = 0x85
+   MAX31865_REG_WRITE_LFT_LSB = 0x86
 
-
-
+   # Configuration Register
+   MAX31865_CONFIG_50HZ_FILTER = 0x01
+   MAX31865_CONFIG_CLEAR_FAULT = 0x02
+   MAX31865_CONFIG_3WIRE       = 0x10
+   MAX31865_CONFIG_ONE_SHOT    = 0x20
+   MAX31865_CONFIG_AUTO        = 0x40
+   MAX31865_CONFIG_BIAS_ON     = 0x80
 
    def __init__(self, wires=2, cs_pin='P9'):
       # initialize ``P9`` in gpio mode and make it an CS output
@@ -40,7 +37,7 @@ MAX31865_CONFIG_BIAS_ON     = 0x80
       self.CS(True)  # init chip select
       self.spi = SPI(0, mode=SPI.MASTER, baudrate=100000, polarity=0, phase=1, firstbit=SPI.MSB)
 
-      # set configuration register 
+      # set configuration register
       config = self.MAX31865_CONFIG_BIAS_ON + self.MAX31865_CONFIG_AUTO + self.MAX31865_CONFIG_CLEAR_FAULT + self.MAX31865_CONFIG_50HZ_FILTER
       if (wires == 3):
           config = config + MAX31865_CONFIG_3WIRE
@@ -77,4 +74,3 @@ MAX31865_CONFIG_BIAS_ON     = 0x80
        # print( 'raw: ', raw)
        temp, RTD = self._RawToTemp(raw)
        return temp
-       
