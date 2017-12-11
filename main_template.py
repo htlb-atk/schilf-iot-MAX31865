aa53264ba5aff46b9d2e8ed1b3f124f4eebaa4df
Kurt Albrecht <kurtalbrecht@Kurts-MacBook-Pro.local>
2017-12-11 21:48:19 +0100

    reorganize files

--- main_template.py
+++ main_template.py
@@ -10,20 +10,19 @@ wlan = WLAN(mode=WLAN.STA)
 nets = wlan.scan()
 for net in nets:
     # if net.ssid == 'WLAN-HTLB':
-    if net.ssid == 'Silkur':
+    if net.ssid == 'WLAN-HTLB':
         print('Network found!')
-        # wlan.connect(net.ssid, auth=(WLAN.WPA2, ''), timeout=5000)
         wlan.connect(net.ssid, auth=(WLAN.WPA2, '***REMOVED***'), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print('WLAN connection succeeded!')
         break
 
-time.sleep(2)
 # create MAX31865 object
 rtd = MAX31865()
 
-thingspeak = MQTTClient(client_id="fe058e3f82b94a968fb8bc105b36dc", server="52.5.134.229", port=1883, user="atkhtlb", password="***REMOVED***")
+# Connect to thingspeak; password= MQTT API Key
+thingspeak = MQTTClient("fe058e3f82b94a968fb8bc105b36dc", "52.5.134.229", port=1883, user="atkhtlb", password="***REMOVED***")
 thingspeak.DEBUG = True
 thingspeak.connect(clean_session=True)
 while True:
